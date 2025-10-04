27216ee6d57535fa9b491e4912b1a6ceffa9ef6f
samuel <legosthes@gmail.com>
2025-10-04 21:13:24 +0800

    feat: add account model and database

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Account(models.Model):
+    class Currency(models.TextChoices):
+        NTD = "NTD", "New Taiwan Dollar"
+        USD = "USD", "United States Dollar"
+        EUR = "EUR", "Euro"
+        GBP = "GBP", "Great British Pound"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, blank=False, null=False)
+    init_amount = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    currency = models.CharField(max_length=30, choices=Currency, default=Currency.NTD)
