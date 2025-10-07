0143d6df0455937ab29c6a315a91e787cb6efb62
samuel <legosthes@gmail.com>
2025-10-07 13:50:30 +0800

    feat: set up record models

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -12,5 +12,5 @@ class Account(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, blank=False, null=False)
-    init_amount = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    init_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     currency = models.CharField(max_length=30, choices=Currency, default=Currency.NTD)
--- apps/records/models.py
+++ apps/records/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.accounts.models import Account
+
 
 # Create your models here.
+class Record(models.Model):
+    class RecordType(models.TextChoices):
+        Expense = "Expense", "Expense"
+        Income = "Income", "Income"
+        Transfer = "Transfer", "Transfer"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    type = models.CharField(max_length=20, choices=RecordType)
