dfca7b28884dc61a99252bea31996d4a0b1e1572
samuel <legosthes@gmail.com>
2025-10-09 15:24:12 +0800

    feat: default account is created when user is registered

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -11,6 +11,10 @@ class Account(models.Model):
         GBP = "GBP", "Great British Pound"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, blank=False, null=False)
+    name = models.CharField(
+        max_length=30,
+        blank=False,
+        default="UserAccount",
+    )
     init_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     currency = models.CharField(max_length=30, choices=Currency, default=Currency.NTD)
