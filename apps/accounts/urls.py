3d8253046800770b8e6d7cb443f9f3d0140524ab
samuel <legosthes@gmail.com>
2025-10-14 17:22:48 +0800

    feat: add account feature

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("accounts/<int:id>/", views.accounts, name="accounts"),
+    path("accounts/<int:user_id>/", views.accounts, name="accounts"),
     path("accounts/new/", views.new_account, name="new_account"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from apps.accounts.models import Account
 from apps.accounts.forms import AccountForm
@@ -6,9 +6,16 @@ from apps.accounts.forms import AccountForm
 
 # Create your views here.
 @login_required
-def accounts(request, id):
-    accounts = Account.objects.filter(user_id=id)
-    return render(request, "pages/accounts.html", {"accounts": accounts})
+def accounts(request, user_id):
+    accounts = Account.objects.filter(user_id=user_id)
+    if request.POST:
+        form = AccountForm(request.POST)
+        account = form.save(commit=False)
+        account.user_id = user_id
+        account.save()
+        return redirect("accounts:accounts", user_id)
+    else:
+        return render(request, "pages/accounts.html", {"accounts": accounts})
 
 
 @login_required
