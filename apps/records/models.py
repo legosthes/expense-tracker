d2e9f36e16bbcb0a75721594d864bf4d35a3e6f4
samuel <legosthes@gmail.com>
2025-10-16 15:01:59 +0800

    update: update record model

--- apps/records/models.py
+++ apps/records/models.py
@@ -10,7 +10,27 @@ class Record(models.Model):
         Income = "Income", "Income"
         Transfer = "Transfer", "Transfer"
 
+    class RecordCategory(models.TextChoices):
+        Restaurants = "Restaurants", "Restaurants"
+        Transportation = "Transportation", "Transportation"
+        Groceries = "Groceries", "Groceries"
+        Shopping = "Shopping", "Shopping"
+        Housing = "Housing", "Housing"
+        Life_Entertainment = "Life & Entertainment", "Life & Entertainment"
+        Others = "Others", "Others"
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    amount = models.DecimalField(
+        max_digits=20,
+        decimal_places=2,
+        blank=False,
+        null=False,
+    )
     type = models.CharField(max_length=20, choices=RecordType)
+    category = models.CharField(
+        max_length=20, choices=RecordCategory, default=RecordCategory.Others
+    )
+    notes = models.TextField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
