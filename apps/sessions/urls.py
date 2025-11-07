b8064141371a994c25bc292826174a8a7ee4a51f
samuel <legosthes@gmail.com>
2025-11-07 14:12:06 +0800

    feat: add home page to website

--- apps/sessions/urls.py
+++ apps/sessions/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "sessions"
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("login/", views.new, name="new"),
     path("create/", views.create, name="create"),
     path("logout/", views.logout_user, name="logout"),
