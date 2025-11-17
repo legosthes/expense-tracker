494f3b428fe6d57ce56773ea8b3c3ea0873db53b
samuel <legosthes@gmail.com>
2025-11-17 23:42:07 +0800

    feat: add env variables

--- budget/settings.py
+++ budget/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-op-wlavnjv(w1el6ug^=94#m)y5u-11*jc(l-x)gq#d#1w(!)-"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False")
